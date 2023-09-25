aa59e25384d957ca5726c52c80d48125ce3c1f37
osneem <oskar.neemre@betssongroup.com>
2023-09-25 18:31:58 +0300

    create order and product

---
+++ Core/Order.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventSourcingImplementation.Core
+{
+    public class Order
+    {  
+        public string UserId { get; set; }
+        public List<Product> Products { get; set; } = new List<Product>();
+
+        public string Address { get; set; }
+        public string PhoneNumber { get; set; }
+    }
+
+    public class Product
+    {
+        public int Id { get; set; }
+    }
+}
