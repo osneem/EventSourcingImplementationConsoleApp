1ec8de6cf04b2676845f3a756d9ef92c031585de
osneem <oskar.neemre@betssongroup.com>
2023-09-25 18:01:25 +0300

    Add events

---
+++ Core/Events.cs
@@ -0,0 +1,11 @@
+﻿namespace EventSourcingImplementation.Core;
+
+public record Event
+{
+    public int Id { get; set; }
+    public string Name => GetType().Name;
+}
+
+public record AddedCart(string UserId): Event;
+public record AddedItemToCart(int ProductId, int Quantity): Event;
+public record AddedShippingInformationCart(string Address, string PhoneNumber): Event;
